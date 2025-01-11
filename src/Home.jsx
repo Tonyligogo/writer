69b30af5c455bcf5c752c81486e746171cf4448a
Tony Ligogo <tonyligogo@gmail.com>
2025-01-11 16:52:35 +0300

    updates again

--- src/Home.jsx
+++ src/Home.jsx
@@ -12,19 +12,19 @@ function Home() {
       <Navbar/>
     </nav>
     <div >
-    <section className="px-4 md:px-8">
+    <section id="#" className="px-4 md:px-8 ">
       <Hero/>
     </section>
-    <section className="px-4 md:px-8">
+    <section id="about" className="px-4 md:px-8 scroll-mt-8">
       <About/>
     </section>
-    <section className="px-4 md:px-8">
+    <section id="services" className="px-4 md:px-8 scroll-mt-24">
       <Services/>
     </section>
-    <section>
+    <section id="my-work" className="scroll-mt-20">
       <MyWork/>
     </section>
-    <section className="px-4 md:px-8">
+    <section id="why-choose-me" className="px-4 md:px-8 scroll-mt-20 ">
       <WhyChooseMe/>
     </section>
     <footer>
--- src/components/MyWork.jsx
+++ src/components/MyWork.jsx
@@ -21,7 +21,7 @@ const subjects = data?.filter(item => !item.isCourse);
   return (
     <div className="px-4 md:px-8 md:py-10 my-10">
       <span className="text-blue-600 text-center md:text-left mb-2 font-semibold block">My Work</span>
-      <h1 className="font-bold text-center md:text-left text-3xl">Talk is Cheap<br className="md:hidden"/><span className='hidden md-'>-</span>Here&apos;s My Work </h1>
+      <h1 className="font-bold text-center md:text-left text-3xl">Talk is Cheap<br className="md:hidden"/> <span className='hidden md:inline-block'>-</span> Here&apos;s My Work </h1>
       <div className="bg-[#1f1f27] border border-[#2a2d38] w-fit flex gap-2 p-1 rounded-md mt-4">
         <button onClick={()=>setCoursesActive('courses')} className={`${coursesActive === 'courses' ? 'bg-dark' : ''} px-2 py-1 rounded-md`}>Courses</button>
         <button onClick={()=>setCoursesActive('subjects')} className={`${coursesActive === 'subjects' ? 'bg-dark' : ''} px-2 py-1 rounded-md`}>Subjects</button>
@@ -36,11 +36,11 @@ const subjects = data?.filter(item => !item.isCourse);
                 return (
                   <li
                     key={item?._id}
-                    className="bg-[#171825] border-[#2a2d38] max-h-[400px] w-full max-w-[500px] p-3 rounded-xl flex-shrink-0"
+                    className="bg-[#171825] border-[#2a2d38] max-h-[400px] w-full max-w-[400px] p-3 rounded-xl flex-shrink-0"
                   >
                     {isVideo ? (
                       <video
-                        className="max-h-[300px] max-w-[300px]"
+                        className="max-h-[300px] w-full"
                         src={item?.imageUrl?.secure_url}
                         controls
                         autoPlay
@@ -79,11 +79,11 @@ const subjects = data?.filter(item => !item.isCourse);
                 return (
                   <li
                     key={item?._id}
-                    className="bg-[#171825] border-[#2a2d38] max-h-[400px] w-full max-w-[500px] p-3 rounded-xl flex-shrink-0"
+                    className="bg-[#171825] border-[#2a2d38] max-h-[400px] w-full max-w-[400px] p-3 rounded-xl flex-shrink-0"
                   >
                     {isVideo ? (
                       <video
-                        className="max-h-[300px] max-w-[300px]"
+                        className="max-h-[300px] w-full"
                         src={item?.imageUrl?.secure_url}
                         controls
                         autoPlay
