3e06e94f4bf022befa536f10c7b3939bcf02166f
Tony Ligogo <tonyligogo@gmail.com>
2025-01-13 15:39:59 +0300

    changed whatsapp number

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ function About() {
             <div className="flex justify-center md:justify-start">
             <button className="bg-gradient-to-r from-sky-500 via-blue-500 to-blue-800 px-8 mt-8 py-2 rounded-md">
             <a
-                href="https://wa.me/+254707494381"
+                href="https://wa.me/+12183107871"
                 target="_blank" 
                 rel="noopener noreferrer"
               >
